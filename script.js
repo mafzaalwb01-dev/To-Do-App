b232444889675ce1361d6cab7518e0f997015475
mafzaalwb01-dev <mafzaalwb01@gmail.com>
2026-06-28 03:26:09 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let input = document.querySelector("#todo-input");
+let button = document.querySelector("#add-btn");
+let show = document.querySelector("#todo-list");
+let jan = document.querySelector(".empty-state");
+let item = document.querySelector(".task-count");
+button.addEventListener("click", () => {
+  let pk = input.value;
+  // console.log(pk);
+  if (pk === "") {
+    let hello = alert("Please Enter here task?");
+    console.log(input.value);
+    return;
+  }
+  let li = document.createElement("li");
+  li.classList.add("todo-item");
+  li.textContent = input.value;
+  show.prepend(li);
+  input.value = "";
+  jan.remove();
+  jani.textContent = `${show.children.length} items`;
+});
